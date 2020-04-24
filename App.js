4f4e9531c80f95fad3f5eaf3f344bec11e1a526c
EndrewCouto <endrewchagas@hotmail.com>
2020-04-23 21:31:06 -0300

    Exercicio4

--- App.js
+++ App.js
@@ -1,59 +1,68 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
-import LembreteItem from './components/LembreteItem'
-import LembreteInput from './components/LembreteInput';
-import Cabecalho from './components/Cabecalho';
-import TelaPrincal from './medidas/TelaPrincipal'
+import React, {useState, useEffect, useRef} from 'react';
+import {Text, View, FlatList } from 'react-native';
+import styles from './style';
+import PageIndex from './pages/index';
+import PageDetail from './pages/PageDetail'
 
 export default function App() {
+  const [contatos, setContatos] = useState([]);
+  const [showPageDetail, setShowPageDetail] = useState(false);
 
-  const [lembretes, setLembretes] = useState([]);
+  const newContacts = useRef([]);
+  const contatoKey = useRef([]);
 
-  const [contadorLembretes, setContadorLembretes] = useState(10);
-
-  const adicionarLembrete = (lembrete, telefone) => {
-    //spread
-    setLembretes (lembretes => {
-      console.log (lembretes);
-      if(contadorLembretes % 2 == 1){
-        setContadorLembretes(contadorLembretes+1); 
-      }else{
-        setContadorLembretes(contadorLembretes+2); 
-      }
-      return [{key: contadorLembretes.toString(), value: "ID: " + contadorLembretes + "\nNome: " + lembrete + "\nTelefone: " + telefone}, ...lembretes];
-    });
-    // setNome("");
-    // setTelefone("");
-    //console.log (lembrete);
-  }
-
-  const removerLembrete = (keyASerRemovida) => {
-    setLembretes(lembretes =>{
-      return lembretes.filter(lembrete => lembrete.key !== keyASerRemovida);
-    })
-  }
+  useEffect(() => {
+    newContacts.current = [...contatos];
+  },[contatos]);
   
 
-  return (
+  /*Valor e prevContatos são recebido da classe index.js 
+  prevContatos é setado no estado de contatos */
+  const atualizarShowPageDetail = (valor, key, prevContatos) => {
+    setContatos([...prevContatos]);
+    setShowPageDetail(valor);
+    contatoKey.current = key;
+  }
 
-    <View>
-      <Cabecalho titulo={'Agenda de Contatos'}/>
-      <View padding={TelaPrincal.TelaPrincipalPadding}>
-        <LembreteInput onAdicionarLembrete={adicionarLembrete}/>
-        <FlatList
-        data={lembretes}
-        renderItem={
-          lembrete => (
-            <LembreteItem 
-              lembrete={lembrete.item.value}
-              chave={lembrete.item.key}
-              onDelete={removerLembrete}
-            />
-          )          
-        }
-      />
-      </View>
+  const pageDetail = (valor) => {
+    setShowPageDetail(valor);
+  }
+
+  const editarContato = (nome, telefone) => {
+    let arr = [...contatos];
+    let index = arr.findIndex((contato => contato.key === contatoKey.current));
+    if(index < 0) {
+      alert('nao existe esse contato para ser editado por favor tente novamente');
+    }
+    let objeto = arr[index];
+    
+
+    objeto.value.nome = nome;
+    objeto.value.telefone = telefone;
+   
+    arr[index] = objeto;
+
+    setContatos([...arr]);
+    setShowPageDetail(false);
+  }
+
+  const localizarContato = (keyDoContato) => {
+    let contact = contatos.filter(contato => contato.key === keyDoContato);
+    let objeto = contact.pop();
+    return objeto;
+}
+  
+  let conteudo = <PageIndex onShowingPageDetail={atualizarShowPageDetail} onUpdateContatos={newContacts.current}/>;
+
+  if(showPageDetail === true) {
+    let objeto = localizarContato(contatoKey.current);
+    conteudo = <PageDetail nome={objeto.value.nome} telefone={objeto.value.telefone} onShowPageDetail={pageDetail} onEditarContato={editarContato}/>
+  }
+
+  return (
+    <View style={styles.container}>
+      {conteudo}
     </View>
-      
+     
   );
 }
